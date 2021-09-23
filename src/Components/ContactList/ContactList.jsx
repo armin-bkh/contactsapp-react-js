ea636d8315fdd8d5dcc5ba3d79792656d31250b3
armin bakhshi <arminbkh0921@yahoo.com>
2021-09-23 20:13:59 +0330

    add many classes for easy styling

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,8 +1,20 @@
-const ContactList = () => {
+const ContactList = ({ contacts }) => {
     return ( 
-        <section>
-            <h1>hello</h1>
-        </section>
+        <>
+        <header className={`border-gray-300 px-3 py-1`}>
+            <h1 className={`text-yellow-400 font-header`}>Contacts</h1>
+        </header>
+        <main>
+            <section>
+                <ul>
+                    { contacts ? 
+                        contacts.map((ct) => ) : 
+                        <h1>Loading...</h1>
+                    }
+                </ul>
+            </section>
+        </main>
+        </>
      );
 }
  
