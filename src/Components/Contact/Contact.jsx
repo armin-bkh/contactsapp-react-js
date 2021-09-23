6f663f7f4708b0a179738754d2a736da741927ea
armin bakhshi <arminbkh0921@yahoo.com>
2021-09-23 23:34:23 +0330

    debug fetching contact in Contact and EditContact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,50 +4,58 @@ import { BiPencil } from "react-icons/bi";
 import { IoReturnUpBack } from "react-icons/io5";
 import { useEffect, useState } from "react";
 
-const Contact = ({ location, history, onDelete }) => {
+const Contact = ({ location, match, history, onDelete }) => {
   const [contact, setContact] = useState(null);
+  const contactId = Number(match.params.id);
 
   useEffect(() => {
     if (location.state) {
       const { contact } = location.state;
       setContact(contact);
       return;
-    } else history.push('/');
+    }
+    if (contactId) {
+      const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+      const index = savedContacts.findIndex((ct) => ct.id === contactId);
+      if (index === -1) history.push("/");
+      const selectedContact = savedContacts[index];
+      setContact(selectedContact);
+    }
   }, []);
 
-  return (
-    contact ? (
-        <article className={`flex flex-col p-5 contact mx-auto h-screen`}>
-        <Link to="/" className={`text-yellow-400 font-header relative inline`}><IoReturnUpBack /></Link>
-          <div className={`mx-auto`}>
-            <FaUserCircle
-              className={`text-gray-300`}
-              style={{ fontSize: "20rem" }}
-            />
-          </div>
-          <div className={`flex justify-between items-center`}>
-            <div>
-              <h1 className={`text-gray-500 font-name mb-2`}>{contact.name}</h1>
-              <h6 className={`text-gray-300 font-email`}>{contact.email}</h6>
-            </div>
-            <Link
-              className={`text-yellow-400 py-1 px-3 rounded-md border-yellow-400 font-name`}
-              to={{ pathname: `/edit-${contact.id}`, state: { contact } }}
-            >
-              <BiPencil />
-            </Link>
-          </div>
+  return contact ? (
+    <article className={`flex flex-col p-5 contact mx-auto h-screen`}>
+      <Link to="/" className={`text-yellow-400 font-header relative inline`}>
+        <IoReturnUpBack />
+      </Link>
+      <div className={`mx-auto`}>
+        <FaUserCircle
+          className={`text-gray-300`}
+          style={{ fontSize: "20rem" }}
+        />
+      </div>
+      <div className={`flex justify-between items-center`}>
+        <div>
+          <h1 className={`text-gray-500 font-name mb-2`}>{contact.name}</h1>
+          <h6 className={`text-gray-300 font-email`}>{contact.email}</h6>
+        </div>
+        <Link
+          className={`text-yellow-400 py-1 px-3 rounded-md border-yellow-400 font-name`}
+          to={{ pathname: `/edit-${contact.id}`, state: { contact } }}
+        >
+          <BiPencil />
+        </Link>
+      </div>
 
-          <button
-            onClick={() => onDelete(contact.id)}
-            className={`py-1 border-red-900 text-red-900 rounded-md bg-transparent font-name mt-auto`}
-          >
-            Remove contact
-          </button>
-        </article>
-      ) : (
-        <h1>Loading...</h1>
-      )
+      <button
+        onClick={() => onDelete(contact.id)}
+        className={`py-1 border-red-900 text-red-900 rounded-md bg-transparent font-name mt-auto`}
+      >
+        Remove contact
+      </button>
+    </article>
+  ) : (
+    <h1>Loading...</h1>
   );
 };
 
--- src/Components/EditContact/EditContact.jsx
+++ src/Components/EditContact/EditContact.jsx
@@ -1,36 +1,45 @@
 import { useEffect, useState } from "react";
 import ContactInput from "../Common/ContactInput/ContactInput";
 
-const EditContact = ({ location, history, onEdit }) => {
+const EditContact = ({ location, match, history, onEdit }) => {
   const [contact, setContact] = useState(null);
   const [error, setError] = useState("");
+  const contactId = Number(match.params.id);
 
   useEffect(() => {
     if (location.state) {
       const { contact } = location.state;
       setContact(contact);
       return;
-    } else history.push("/");
+    }
+    if (contactId) {
+      const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+      const index = savedContacts.findIndex((ct) => ct.id === contactId);
+
+      if (index === -1) history.push("/");
+      const selectedContact = savedContacts[index];
+      setContact(selectedContact);
+    }
   }, []);
 
   const changeHandler = (e) => {
     setError("");
     setContact({
-        ...contact,
-        [e.target.name]: e.target.value,
-    })
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const submitHandler = (e) => {
-      e.preventDefault();
+    e.preventDefault();
     if (contact.name || contact.email) {
       if (contact.name) {
         if (contact.email) {
-            onEdit(contact.id, contact);
-            history.push('/');
-        } else setError("Contact email is necessary")
-      } else setError("Conatct name is necessary")
-    } else setError("All contact information is necessary")
+          onEdit(contact.id, contact);
+          history.push("/");
+        } else setError("Contact email is necessary");
+      } else setError("Conatct name is necessary");
+    } else setError("All contact information is necessary");
   };
   return (
     <>
@@ -57,7 +66,9 @@ const EditContact = ({ location, history, onEdit }) => {
               placeholder="Email..."
               onChange={changeHandler}
             />
-            {error && <h1 className={`font-email text-red-900 mt-3`}>{error}</h1>}
+            {error && (
+              <h1 className={`font-email text-red-900 mt-3`}>{error}</h1>
+            )}
             <button
               type="submit"
               className={`w-full py-1 mt-auto bg-yellow-400 text-gray-800`}
